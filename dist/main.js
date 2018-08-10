6ef55abc4afb61912bfd8f8ec8408a79478d55b8
ACharLuk <acharluk@gmail.com>
2018-08-10 17:25:41 +0200

    Remove compiled js

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tokenizer_1 = require("./tokenizer");
-const code = "12 + 97 - 26";
-let tokenizer = new tokenizer_1.Tokenizer(code);
-console.log(tokenizer.tokens);
