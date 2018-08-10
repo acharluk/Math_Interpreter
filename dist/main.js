75f64ec46d4fe9887fc19d212865606e7d596003
ACharLuk <acharluk@gmail.com>
2018-08-10 17:25:04 +0200

    Test

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("Hello world!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const tokenizer_1 = require("./tokenizer");
+const code = "12 + 97 - 26";
+let tokenizer = new tokenizer_1.Tokenizer(code);
+console.log(tokenizer.tokens);
--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+import { Tokenizer } from "./tokenizer";
+
+const code = "12 + 97 - 26";
+
+let tokenizer = new Tokenizer(code);
+console.log(tokenizer.tokens);
