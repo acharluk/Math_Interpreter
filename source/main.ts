7a2deda1dd3add94ddb72210889757afff68545a
ACharLuk <acharluk@gmail.com>
2018-08-12 13:47:21 +0200

    Convert to LF before tokenizing

--- source/main.ts
+++ source/main.ts
@@ -4,7 +4,7 @@ import { Interpreter } from "./interpreter";
 import { readFileSync } from "fs";
 
 const fileData = readFileSync('test.op');
-const code = fileData.toString();
+const code = fileData.toString().replace(/\r\n/g, '\n');
 
 let tokenizer = new Tokenizer(code);
 let interpreter = new Interpreter(tokenizer.tokens);
